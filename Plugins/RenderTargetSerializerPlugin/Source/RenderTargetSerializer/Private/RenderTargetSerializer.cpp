af920b86255136a19ba1a3fd9d079f3c2858fe43
Alex <chuklov@gmail.com>
2024-07-31 21:20:07 -0400

    Testing for 5 .4

--- Plugins/RenderTargetSerializerPlugin/Source/RenderTargetSerializer/Private/RenderTargetSerializer.cpp
+++ Plugins/RenderTargetSerializerPlugin/Source/RenderTargetSerializer/Private/RenderTargetSerializer.cpp
@@ -1,22 +1,106 @@
-/*
-	This code was written by Alexander Chadfield
-	
-	Plugin created by Alexander Chadfield
-*/
-
-
+#include "RenderTargetSerializerBPLibrary.h"
 #include "RenderTargetSerializer.h"
+#include "EngineUtils.h"
+#include "Math/Vector.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "Engine/Texture2D.h"
+#include "Rendering/Texture2DResource.h"
+#include "RenderUtils.h"
 
-#define LOCTEXT_NAMESPACE "FRenderTargetSerializerModule"
-
-void FRenderTargetSerializerModule::StartupModule()
+URenderTargetSerializerBPLibrary::URenderTargetSerializerBPLibrary(const FObjectInitializer& ObjectInitializer)
+    : Super(ObjectInitializer)
 {
+
 }
 
-void FRenderTargetSerializerModule::ShutdownModule()
-{	
+TArray<FVector> URenderTargetSerializerBPLibrary::SerializeRenderTarget(UTextureRenderTarget2D* RenderTarget)
+{
+    // Convert pixel data to array of vectors
+    TArray<FVector> PixelVectors;
+
+    if (!RenderTarget)
+    {
+        return PixelVectors;
+    }
+
+    // Get the render target size
+    int32 Width = RenderTarget->SizeX;
+    int32 Height = RenderTarget->SizeY;
+
+    // Allocate memory for pixel data
+    TArray<FColor> PixelData;
+    PixelData.Init(FColor::Black, Width * Height);
+
+    // Read pixel data from the render target
+    FRenderTarget* RenderTargetResource = RenderTarget->GameThread_GetRenderTargetResource();
+    FReadSurfaceDataFlags ReadPixelFlags;
+    ReadPixelFlags.SetLinearToGamma(false); // Ensure no colour space conversion
+    RenderTargetResource->ReadPixels(PixelData, ReadPixelFlags);
+
+    for (int32 y = 0; y < Height; ++y)
+    {
+        for (int32 x = 0; x < Width; ++x)
+        {
+            // Convert pixel colour to vector format (XYZ = RGB)
+            FColor PixelColor = PixelData[y * Width + x];
+            FVector PixelVector(PixelColor.R, PixelColor.G, PixelColor.B);
+
+            // Add pixel vector to the array
+            PixelVectors.Add(PixelVector);
+        }
+    }
+
+    return PixelVectors; //SerializedData;
 }
 
-#undef LOCTEXT_NAMESPACE
-	
-IMPLEMENT_MODULE(FRenderTargetSerializerModule, RenderTargetSerializer)
+UTexture2D* URenderTargetSerializerBPLibrary::DeserializeRenderTarget(const TArray<FVector>& PixelVectors, int32 Width, int32 Height)
+{
+    if (Width <= 0 || Height <= 0 || PixelVectors.Num() != Width * Height)
+    {
+        return nullptr;
+    }
+
+    // Create a texture 2D to store the pixel data
+    UTexture2D* Texture2D = UTexture2D::CreateTransient(Width, Height, PF_B8G8R8A8);
+    if (!Texture2D)
+    {
+        return nullptr;
+    }
+
+    // Lock the texture for writing
+    FTexture2DResource* Texture2DResource = static_cast<FTexture2DResource*>(Texture2D->GetResource());
+    if (!Texture2DResource)
+    {
+        return nullptr;
+    }
+
+    ENQUEUE_RENDER_COMMAND(UpdateTextureData)(
+        [Texture2DResource, PixelVectors, Width, Height](FRHICommandListImmediate& RHICmdList)
+    {
+        // Lock the texture for writing
+        uint32 DestStride;
+        uint8* DestData = (uint8*)RHILockTexture2D(Texture2DResource->GetTexture2DRHI(), 0, RLM_WriteOnly, DestStride, false);
+
+        // Copy pixel data into the texture after converting to FColor
+        for (int32 y = 0; y < Height; ++y)
+        {
+            FColor* RowData = (FColor*)(DestData + y * DestStride);
+            for (int32 x = 0; x < Width; ++x)
+            {
+                int32 Index = y * Width + x;
+                FVector PixelVector = PixelVectors[Index];
+                FColor PixelColor(FMath::Clamp(int32(PixelVector.X), 0, 255), FMath::Clamp(int32(PixelVector.Y), 0, 255), FMath::Clamp(int32(PixelVector.Z), 0, 255), 255);
+                RowData[x] = PixelColor;
+            }
+        }
+
+        // Unlock the texture
+        RHIUnlockTexture2D(Texture2DResource->GetTexture2DRHI(), 0, false);
+    }
+    );
+
+    // Update the texture resource
+    Texture2D->UpdateResource();
+
+    return Texture2D;
+}
--- Plugins/RenderTargetSerializerPlugin/Source/RenderTargetSerializer/Public/RenderTargetSerializer.h
+++ Plugins/RenderTargetSerializerPlugin/Source/RenderTargetSerializer/Public/RenderTargetSerializer.h
@@ -1,18 +1,26 @@
 /*
 	This code was written by Alexander Chadfield
-	
+
 	Plugin created by Alexander Chadfield
 */
 
 #pragma once
 
-#include "Modules/ModuleManager.h"
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "Engine/TextureRenderTarget2D.h"
+#include "RenderTargetSerializerBPLibrary.generated.h"
 
-class FRenderTargetSerializerModule : public IModuleInterface
+UCLASS()
+class URenderTargetSerializerBPLibrary : public UBlueprintFunctionLibrary
 {
+	GENERATED_UCLASS_BODY()
+
 public:
 
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Serialize Render Target", Keywords = "RenderTargetSerializer"), Category = "Render Target Serialization")
+		static TArray<FVector> SerializeRenderTarget(UTextureRenderTarget2D* RenderTarget);
+
+	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Deserialize Render Target", Keywords = "RenderTargetSerializer"), Category = "Render Target Serialization")
+		static UTexture2D* DeserializeRenderTarget(const TArray<FVector>& PixelVectors, int32 Width, int32 Height);
 };

fa58afeae02abc8f3c5cb7188ddb0df1e1ffb0ef
Alex <chuklov@gmail.com>
2025-02-28 10:55:04 -0500

    add documentation

--- Plugins/RenderTargetSerializer/Source/RenderTargetSerializer/Public/RenderTargetSerializerBPLibrary.h
+++ Plugins/RenderTargetSerializer/Source/RenderTargetSerializer/Public/RenderTargetSerializerBPLibrary.h
@@ -18,9 +18,23 @@ class URenderTargetSerializerBPLibrary : public UBlueprintFunctionLibrary
 
 public:
 
+	/**
+	* This method will serialize a Render Target from RGBA to Array of Integers
+	* 
+	* @param TextureRenderTarget2D 
+	*/
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Serialize RT RGBA to Bytes", Keywords = "Render Target Serializer v2"), Category = "Render Target Serialization")
 		static TArray<uint8> SerializeRenderTarget(UTextureRenderTarget2D* RenderTarget);
 
+
+
+	/**
+	* This method will deserialize Array of int8 into a Render Target to RGBA 
+	*
+	* @param Array of Int8
+	* @param Width of the Render Target
+	* @param Height of the Render Target
+	*/
 	UFUNCTION(BlueprintCallable, meta = (DisplayName = "Deserialize RT from Bytes to RGBA", Keywords = "Render Target Serializer v2"), Category = "Render Target Serialization")
 		static UTexture2D* DeserializeRenderTarget(const TArray<uint8>& PixelBytes, int32 Width, int32 Height);
 };
